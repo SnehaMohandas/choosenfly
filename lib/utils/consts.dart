749043c1df63ff4a6250996c33a1039050ec948d
SnehaMohandas <snehamohan880@gmail.com>
2024-01-04 15:46:12 +0530

    modify

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
 
-String baseUrl = "http://3.141.66.171/ConnectWorld/";
-String imgUrl = "http://3.141.66.171/";
-
-String header = token!;
-String agentId = userId!;
-
-String? name;
-String? token;
-
-String? userId;
-String? companyName;
-String? agentProfile;
